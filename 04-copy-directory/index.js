c482a2fb6f136008d45efadcb5096b49f94f5150
Gennadii Shpreer <intellegento@gmail.com>
2023-05-05 12:58:41 +0600

    feat: fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
 
+const fs = require('fs');
+const promises = fs.promises;
+const path = require('path');
+
+const pathFolder = path.resolve(__dirname, 'files');
+const pathCopyFolder = path.resolve(__dirname, 'files-copy');
+promises.mkdir(pathCopyFolder, { recursive: true });
+const copyFolder = promises.readdir(pathCopyFolder);
+
+function clearFolder() {
+  copyFolder.then((files) =>
+    files.forEach((file) => {
+      const pathToCopyFile = path.resolve(pathCopyFolder, file);
+      promises.unlink(pathToCopyFile);
+    })
+  );
+}
+
+clearFolder();
+
+const objInFolder = promises.readdir(pathFolder);
+objInFolder.then((files) =>
+  files.forEach((file) => {
+    const pathToFile = path.resolve(pathFolder, file);
+    const pathToCopyFile = path.resolve(pathCopyFolder, file);
+    promises.copyFile(pathToFile, pathToCopyFile);
+  })
+);
+
+console.log("*****Copy completed*****");
